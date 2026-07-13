b0d9c90d3c010d70197551079938f9e2772c81ea
chayannigam09 <chayannigam09@gmail.com>
2026-07-13 16:22:38 +0530

    Kashmir destination added

--- src/tours/index.ts
+++ src/tours/index.ts
@@ -1,10 +1,13 @@
 import pachmarhi from './pachmarhi/data.json';
 import maheshwar from './maheshwar/data.json';
 import chardham from './cahrdham/data.json'
+import kashmir from './Kashmir/data.json';
+
 export const tours = [
   pachmarhi,
   maheshwar,
   chardham,
+  kashmir,
   pachmarhi,
   maheshwar,
   pachmarhi,
