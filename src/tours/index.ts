2d952ce7421cfd0b09bbcabee9283a3251c39511
chayannigam09 <chayannigam09@gmail.com>
2026-07-14 19:28:31 +0530

    Mandu added

--- src/tours/index.ts
+++ src/tours/index.ts
@@ -2,10 +2,12 @@ import pachmarhi from './pachmarhi/data.json';
 import maheshwar from './maheshwar/data.json';
 import chardham from './cahrdham/data.json'
 import kashmir from './Kashmir/data.json';
+import mandu from './mandu/data.json'
 
 export const tours = [
-  pachmarhi,
   maheshwar,
+  mandu,
+  pachmarhi,
   chardham,
   kashmir,
   pachmarhi,
